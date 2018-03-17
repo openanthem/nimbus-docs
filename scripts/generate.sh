0c468b2a35b2aa3abaeb1310b9c5c0660276c853
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2018-03-17 14:24:39 -0500

    Updating generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-BUILD_DIR="build/1.0.0.M4/"
-ROOT_ADOC_PATH="src/Reference/"
+# Set variables
+DOCUMENTATION_VERSION="1.0.0.M4"
+BUILD_DIR="build/${DOCUMENTATION_VERSION}"
+ROOT_ADOC_PATH="src/Reference"
 ROOT_ADOC_FILENAME="default.adoc"
-ROOT_ADOC="${ROOT_ADOC_PATH}${ROOT_ADOC_FILENAME}"
+ROOT_ADOC="${ROOT_ADOC_PATH}/${ROOT_ADOC_FILENAME}"
 
 # Copy dependencies
-cp -R "${ROOT_ADOC_PATH}tocbot-3.0.2" "$BUILD_DIR"
+mkdir "${BUILD_DIR}"
+cp -R "${ROOT_ADOC_PATH}/tocbot-3.0.2" "$BUILD_DIR/"
 
 # Create the HTML
 asciidoctor -d book "$ROOT_ADOC" -D "$BUILD_DIR" -a toc=left -a docinfo=shared
