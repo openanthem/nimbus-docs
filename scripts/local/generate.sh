9b87f0bc175cacee3a40976d2e6b19001167baf3
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2018-06-26 09:02:08 -0500

    Local build script improvements
    Can now pass -V to specify a VERSION representing the framework version. Asciidoc outputs now build to 'build/VERSION' and now produce both .html and .pdf files

--- scripts/local/generate.sh
+++ scripts/local/generate.sh
@@ -2,12 +2,23 @@
 
 # Set variables
 DOCUMENTATION_VERSION="latest"
-BUILD_DIR="build/${DOCUMENTATION_VERSION}"
 SOURCE_PATH="src"
 IMAGES_PATH="${SOURCE_PATH}/images"
 ROOT_ADOC_PATH="${SOURCE_PATH}/Reference"
 ROOT_ADOC_FILENAME="default.adoc"
 ROOT_ADOC="${ROOT_ADOC_PATH}/${ROOT_ADOC_FILENAME}"
+
+# Collect user args
+while getopts V: option
+do
+case "${option}"
+in
+V) DOCUMENTATION_VERSION=${OPTARG};;
+esac
+done
+
+# Set varibles
+BUILD_DIR="build/${DOCUMENTATION_VERSION}"
 
 # Copy dependencies
 mkdir -p "${BUILD_DIR}"
@@ -15,4 +26,9 @@ cp -R "${ROOT_ADOC_PATH}/tocbot-3.0.2" "$BUILD_DIR/"
 cp -R "${IMAGES_PATH}" "$BUILD_DIR/"
 
 # Create the HTML
-asciidoctor -d book "$ROOT_ADOC" -D "$BUILD_DIR" -a toc=left -a docinfo=shared
+echo "Building asciidoctor HTML to $BUILD_DIR/index.html..."
+asciidoctor -d book "$ROOT_ADOC" -D "$BUILD_DIR" -a toc=left -a docinfo=shared -o "index.html"
+
+# Create the PDF
+echo "Building asciidoctor PDF to $BUILD_DIR/$DOCUMENTATION_VERSION.pdf..."
+asciidoctor-pdf -d book "$ROOT_ADOC" -D "$BUILD_DIR" -o "$DOCUMENTATION_VERSION.pdf" -a imagesdir="../images"
