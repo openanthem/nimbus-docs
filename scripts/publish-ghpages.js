a0aef1792e1e741be29a7f3daaff2891000ce8cd
lopezton-anthem <antoniotony.lopez@anthem.com>
2019-07-08 15:56:05 -0500

    gh-pages publish

--- scripts/publish-ghpages.js
+++ scripts/publish-ghpages.js
@@ -4,4 +4,28 @@ const NPM_VERSION = process.env.npm_package_version;
 const RELEASE_TYPE = process.env.npm_package_release_type;
 const VERSION = `${NPM_VERSION}.${RELEASE_TYPE}`;
 
+console.log('Generating HTML content from asciidoc...');
 shell.exec(`./scripts/generate.sh -V ${VERSION}`);
+
+var ghpages = require('gh-pages');
+
+var options = {
+    src: '**/*',
+    branch: 'gh-pages',
+    dest: `${VERSION}`,
+    message: `${VERSION} updates`,
+};
+
+var isRelease = 'BUILD-SNAPSHOT' !== RELEASE_TYPE;
+var shouldDeploy = true;
+
+if (isRelease) {
+    options.tag = `v${VERSION}`;
+}
+
+if (shouldDeploy) {
+    console.log('Publishing HTML content to gh-pages...');
+    ghpages.publish(`dist/${VERSION}`, options, function(err) {
+        throw err;
+    });
+}
